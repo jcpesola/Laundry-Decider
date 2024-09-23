3cb751e93fe500d485a7eedab4ecd1da71ef404f
Julia Pesola <jcpesola@gmail.com>
2024-09-22 20:20:57 -0400

    Added read_laundry_store_price route

--- backend/app/routes/main_routes.py
+++ backend/app/routes/main_routes.py
@@ -15,7 +15,44 @@ def read_laundry_store_prices(store_id):
 
 @main_bp.route('/read_laundry_store_price', methods=['GET'])
 def read_laundry_store_price(store_id):
-     pass
+     #Query laundry store, wash_and_fold price, and dry_cleaning_price
+     store = LaundryStore.query.get_or404(id)
+     wash_and_fold = store.wash_and_fold_price
+     dry_cleaning = store.dry_cleaning_price
+
+     #Prepare info to return in JSON format
+     pricing_info = {
+          "store_name": store.name,
+          "wash_and_fold_price": {
+               "min_price": wash_and_fold.min_price, 
+               "min_pounds": wash_and_fold.min_pounds, 
+               "price_per_pound": wash_and_fold.price_per_pound
+               },
+          "dry_cleaning_price": {
+               "dress": dry_cleaning.dress,
+               "sweater": dry_cleaning.sweater,
+               "skirt": dry_cleaning.skirt,
+               "shorts": dry_cleaning.shorts,
+               "outer_jacket": dry_cleaning.outer_jacket,
+               "coat": dry_cleaning.coat,
+               "jump_suit": dry_cleaning.jump_suit,
+               "launder_shirt": dry_cleaning.launder_shirt,
+               "dry_clean_shirt": dry_cleaning.dry_clean_shirt,
+               "blouse": dry_cleaning.blouse,
+               "pants": dry_cleaning.pants,
+               "suit_jacket": dry_cleaning.suit_jacket,
+               "two_piece_suit": dry_cleaning.two_piece_suit,
+               "robe": dry_cleaning.robe,
+               "scarf": dry_cleaning.scarf,
+               "tie": dry_cleaning.tie,
+               "tuxedo": dry_cleaning.tuxedo,
+               "three_piece_suit": dry_cleaning.three_piece_suit
+          }
+     }
+
+     #Return pricing info as JSON
+     return jsonify(pricing_info)
+
 
 #API 3: Get list of laundry stores sorted by price
 
